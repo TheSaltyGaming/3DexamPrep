87d6e66c188fdb5262874fa68183d8742bbfeb26
Anders <andersaugust@outlook.com>
2024-05-10 12:04:27 +0200

    llllll

--- 3Dexam/3DexamBackup.cpp
+++ 3Dexam/3DexamBackup.cpp
@@ -1,7 +1,11 @@
 #include "Shader.h"
 #include "ShaderFileLoader.h"
 #include <iostream>
-#include "glm/mat4x3.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Camera.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -10,14 +14,23 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-// std::string vfs = ShaderLoader::LoadShaderFromFile("Triangle.vert");
-// std::string fs = ShaderLoader::LoadShaderFromFile("Triangle.frag");
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+
+std::string vfs = ShaderLoader::LoadShaderFromFile("VertShader.vert");
+std::string fs = ShaderLoader::LoadShaderFromFile("FragmentShader.frag");
+
+Camera MainCamera;
+// Shader ourShader(vfs.c_str(), fs.c_str());
+
+void CameraView(std::vector<unsigned> shaderPrograms, glm::mat4 trans, glm::mat4 projection);
 
 
 int main()
 {
     std::cout << "Vfs " << vfs.c_str() << std::endl;
     std::cout << "fs " << fs.c_str() << std::endl;
+    
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
@@ -48,9 +61,8 @@ int main()
 
     // build and compile our shader program
     // ------------------------------------
-
-    //Shader ourShader("Triangle.vert", "Triangle.frag"); // you can name your shader files however you like
-    Shader ourShader("VertShaderOld.vert", "FragShaderOld.frag"); // you can name your shader files however you like
+    
+    Shader ourShader("VertShader.vert", "FragmentShader.frag"); // you can name your shader files however you 
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -76,6 +88,15 @@ int main()
     // color attribute
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    glUniform3f(glGetUniformLocation(ourShader.ID, "objectColor"), 1.0f, 0.5f, 0.31f);
+    glUniform3f(glGetUniformLocation(ourShader.ID, "lightColor"), 1.0f, 1.0f, 1.0f);
+    glUniform3f(glGetUniformLocation(ourShader.ID, "lightPos"), 1.f, 1.f, 1.f);
+    glUniform3f(glGetUniformLocation(ourShader.ID, "viewPos"), MainCamera.cameraPos.x, MainCamera.cameraPos.y, MainCamera.cameraPos.z);
+
+    glEnable(GL_DEPTH_TEST)
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
@@ -123,6 +144,30 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    // Camera Code
+    glm::vec3 cameraFrontXZ = glm::normalize(glm::vec3(MainCamera.cameraFront.x, 0.0f, MainCamera.cameraFront.z));
+    float cameraSpeed = 2.5f * deltaTime;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        MainCamera.cameraPos += cameraSpeed * cameraFrontXZ;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        MainCamera.cameraPos -= cameraSpeed * cameraFrontXZ;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        MainCamera.cameraPos -= glm::normalize(glm::cross(MainCamera.cameraFront, MainCamera.cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        MainCamera.cameraPos += glm::normalize(glm::cross(MainCamera.cameraFront, MainCamera.cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        MainCamera.cameraPos += cameraSpeed * MainCamera.cameraUp; // Move camera up
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        MainCamera.cameraPos -= cameraSpeed * MainCamera.cameraUp; // Move camera down
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+    {
+        ;
+        MainCamera.cameraPos.y += 0.01;
+    }
+
+    
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -132,4 +177,28 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+
+void CameraView(std::vector<unsigned> shaderPrograms, glm::mat4 trans, glm::mat4 projection)
+{
+    for (unsigned shaderProgram : shaderPrograms)
+    {
+        glUseProgram(shaderProgram);
+
+        int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        MainCamera.tick();
+
+        glm::mat4 view;
+        view = glm::lookAt(MainCamera.cameraPos, MainCamera.cameraPos + MainCamera.cameraFront, MainCamera.cameraUp);
+
+        int viewLoc = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        // Pass the transformation matrix to the vertex shader
+        unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+    }
 }
